61925e37ca120d4001437559d6bc92a3206de2e6
Priya Gupta <prigup@Priyas-MacBook-Pro.local>
2020-03-15 22:46:52 +0530

    Add templates:

--- app.py
+++ app.py
@@ -4,12 +4,48 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('home.html')
+    return render_template('index.html')
 
 @app.route('/about')
 def about():
     return render_template('about.html')
 
-@app.route('/sample')
-def sample():
-    return render_template('sample.html')
+@app.route('/news1')
+def news1():
+    return render_template('news1.html')
+
+@app.route('/news2')
+def news2():
+    return render_template('news2.html')
+
+@app.route('/news3')
+def news3():
+    return render_template('news3.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+@app.route('/login_student')
+def login_student():
+    return render_template('login_student.html')
+
+@app.route('/login_teacher')
+def login_teacher():
+    return render_template('login_teacher.html')
+
+@app.route('/student_dashboard')
+def student_dashboard():
+    return render_template('student_dashboard.html')
+
+@app.route('/upload_content')
+def upload_content():
+    return render_template('upload_content.html')
+
+@app.route('/view_content')
+def view_content():
+    return render_template('view_content.html')
+
+@app.route('/view_submission')
+def view_submission():
+    return render_template('view_submission.html')
